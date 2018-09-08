a6bd3e359236fb121c27fc32232a7a97740d2130
William <willi4m@zju.edu.cn>
2018-09-08 17:04:29 +0800

    ...

--- notes/urls.py
+++ notes/urls.py
@@ -1,8 +1,5 @@
 from django.urls import path
 
-from django.conf import settings
-from django.conf.urls.static import static
-
 from . import views
 
 
@@ -10,4 +7,4 @@ urlpatterns = [
   path('', views.index, name='index'),
   path('<int:note_id>/', views.index, name='index'),
   path('avatar/', views.avatar, name='avatar'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
